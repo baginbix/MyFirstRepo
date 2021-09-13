309a1abb3b155bba8dd969fa7ac27333aa8d72e3
FiriyalM <43432879+FiriyalM@users.noreply.github.com>
2021-09-13 09:54:47 +0200

    Lagt till filer

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ny_mapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(true)
+            {
+                int i; // Deklarering
+                i = 42;
+                i++;
+            }
+            
+        }
+    }
+}
